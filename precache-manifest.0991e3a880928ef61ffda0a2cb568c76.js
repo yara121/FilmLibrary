1df01c0bbde0b0292d84be29b44a34b8c34cb79b
yara121 <yara767@gmail.com>
2023-09-16 22:02:18 +0300

    Updates

---
+++ precache-manifest.0991e3a880928ef61ffda0a2cb568c76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8832036ea84a3a7c0f11cd3deb8fa10",
+    "url": "/FilmLibrary/index.html"
+  },
+  {
+    "revision": "84566ae813093e33c81c",
+    "url": "/FilmLibrary/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "84566ae813093e33c81c",
+    "url": "/FilmLibrary/static/js/2.4cb15afa.chunk.js"
+  },
+  {
+    "revision": "ac9c09499ba9e4be740729c250300ba4",
+    "url": "/FilmLibrary/static/js/2.4cb15afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40fc92370920fdac771d",
+    "url": "/FilmLibrary/static/js/main.4b54f811.chunk.js"
+  },
+  {
+    "revision": "a8379b726364d6577be1",
+    "url": "/FilmLibrary/static/js/runtime-main.b25840f5.js"
+  },
+  {
+    "revision": "9fd004153f98ae9f8907cbfd12fe817c",
+    "url": "/FilmLibrary/static/media/logo.9fd00415.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Netflix-clone/precache-manifest.5f019fb9e709c69acdfca8d49448786e.js"
+  "/FilmLibrary/precache-manifest.0991e3a880928ef61ffda0a2cb568c76.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Netflix-clone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FilmLibrary/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
